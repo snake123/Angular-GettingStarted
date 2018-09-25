1c3c240416466793d02b1e4ff58c52960e3a1a19
unknown <you@example.com>
2018-09-25 21:15:03 +0200

    first

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:  `
+  <pm-products></pm-products>
+
+  `
+  
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'ACME: Getting Started';
 }
